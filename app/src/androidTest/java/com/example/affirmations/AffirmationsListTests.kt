ae3f7fcaaeacf2ca978f065e499d9c1c2dcdaf68
Viktor Stashchuk <vstashchuk@reenbit.com>
2023-11-03 11:09:22 +0200

    Add test for RecyclerView

--- app/src/androidTest/java/com/example/affirmations/AffirmationsListTests.kt
+++ app/src/androidTest/java/com/example/affirmations/AffirmationsListTests.kt
@@ -1,5 +1,12 @@
 package com.example.affirmations
 
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
@@ -13,6 +20,13 @@ class AffirmationsListTests {
 
     @Test
     fun scroll_to_item(){
-        
+        onView(withId(R.id.recycler_view)).perform(
+            RecyclerViewActions
+                .scrollToPosition<RecyclerView.ViewHolder>(9)
+        )
+
+        onView(withText(R.string.affirmation10))
+            .check(matches(isDisplayed())
+            )
     }
 }
