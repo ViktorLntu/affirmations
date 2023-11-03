875a811b85ac47cc31a1f687a7637b2971d9292d
Viktor Stashchuk <vstashchuk@reenbit.com>
2023-11-03 11:16:38 +0200

    Add test for Adapter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.test.espresso:espresso-contrib:3.4.0")
+    testImplementation("org.mockito:mockito-core:3.12.4")
 }
